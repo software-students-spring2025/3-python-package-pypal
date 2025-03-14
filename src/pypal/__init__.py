0a219aac051d0f9a0c31ecb5dcfe386114388f18
AllenNi66 <yn2178@nyu.edu>
2025-03-14 15:19:17 -0400

    init

--- src/pypal/__init__.py
+++ src/pypal/__init__.py
@@ -1,5 +1,1 @@
-# __init__.py
-from .names import generate_name
-from .addresses import generate_address
-from .phone_numbers import generate_phone_number
-from .profiles import generate_profile
+from generator import generate_name, generate_address, generate_phone_number, generate_profile
