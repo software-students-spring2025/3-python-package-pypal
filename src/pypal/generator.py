0294a7a46d2ae8d6a4fbfe91c69ed0a72d564d28
JasonLIN0226 <xl4358@nyu.edu>
2025-03-14 13:57:43 -0400

    Files set up

---
+++ src/pypal/generator.py
@@ -0,0 +1,4 @@
+"""
+This module provides a function to retrieve a pseudo-random line from Lewis Carroll's poem "The Jabberwocky".
+Read more about Jabberwocky here: https://en.wikipedia.org/wiki/Jabberwocky
+"""
